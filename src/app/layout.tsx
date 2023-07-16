a06cf5a91a12dabb1171b4e749f6a9f7f1ae794e
iorise <riofirmansyah1805@gmail.com>
2023-07-16 12:14:19 +0700

    add clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,32 @@
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'E-Commerce admin',
-  description: 'E-Commerce admin',
-}
+  title: "E-Commerce admin",
+  description: "E-Commerce admin",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}> <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {" "}
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             {children}
-          </ThemeProvider></body>
-    </html>
-  )
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
