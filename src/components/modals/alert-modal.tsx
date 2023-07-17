eb43635caa51db36863b7cc67b2aa07d5ee9a03b
iorise <riofirmansyah1805@gmail.com>
2023-07-17 15:50:03 +0700

    add alert modal

---
+++ src/components/modals/alert-modal.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import * as React from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+}
+
+export function AlertModal({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading,
+}: AlertModalProps) {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <Modal
+      title="Are you sure?"
+      description="This action cannot be undone"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="w-full flex items-center  pt-6 space-x-2">
+        <Button disabled={loading} variant="outline" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} variant="destructive" onClick={onConfirm}>Continue</Button>
+      </div>
+    </Modal>
+  );
+}
